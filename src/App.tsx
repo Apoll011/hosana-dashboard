a4f482dbd27661027623a1b8737b17d0ba163ba6
apoll011 <tiagorobotik@gmail.com>
2026-08-04 23:30:29 +0100

    Add @statsig/react-bindings for feature flag management; implement StatsigWrapper in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import { configureApiClient } from "@hosanna/shared";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
+import { AuthProvider, useAuth } from "./contexts/AuthContext";
 import { SyncProvider } from "./contexts/SyncContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { AppRoutes } from "./routes/AppRoutes";
+import { StatsigProvider, useClientAsyncInit } from "@statsig/react-bindings";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,25 +18,45 @@ const queryClient = new QueryClient({
   },
 });
 
+function StatsigWrapper({ children }: { children: React.ReactNode }) {
+  const { user } = useAuth();
+  const id = user?.id || "a-user";
+  const { client } = useClientAsyncInit(
+    "client-4459XEXCHZyP192QOlIwzRAffGVP9zfS33rnXpdquAI",
+    { userID: id }
+  );
+
+  return (
+    <StatsigProvider client={client} loadingComponent={<div>Loading...</div>}>
+      {children}
+    </StatsigProvider>
+  );
+}
+
 export default function App() {
   configureApiClient(
     localStorage.getItem("server_url") ||
       import.meta.env.VITE_API_URL ||
       "/api",
   );
+
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <ThemeProvider>
-          <SyncProvider>
-            <BrowserRouter>
-              <Analytics />
-              <SpeedInsights />
-              <AppRoutes />
-            </BrowserRouter>
-          </SyncProvider>
-        </ThemeProvider>
+        <StatsigWrapper>
+          <ThemeProvider>
+            <SyncProvider>
+              <BrowserRouter>
+                <Analytics />
+                <SpeedInsights />
+                <AppRoutes />
+              </BrowserRouter>
+            </SyncProvider>
+          </ThemeProvider>
+        </StatsigWrapper>
       </AuthProvider>
     </QueryClientProvider>
   );
 }
+
+
